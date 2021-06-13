bcfec24cb2e14303b1d47e250a7a97fd2879f955
shradh90 <shradhad1996@gmail.com>
2021-06-13 12:14:42 +0530

    swapping nibbles and convert it back into decimal

---
+++ swap.java
@@ -0,0 +1,31 @@
+package tobinary;
+import java.util.Scanner;
+public class tobinary {
+	 public static void main(String[] args) {
+	        System.out.println("enter decimal number : ");
+			Scanner sc = new Scanner(System.in);
+			int dec = sc.nextInt();
+			long bin = convertdectobin(dec);
+			System.out.println("converted binary number : " + bin);
+			long swap1 = swaping(bin);
+			System.out.println("after swapping nibbles number in decimal : " + swap1);
+	 }
+	 public static long convertdectobin(int dec) {
+		 long binary = 0000;
+		 int rem;
+		 int i = 1;
+		 int step = 1;
+				while(dec != 0) {
+					rem = dec % 2;
+					dec = dec / 2;
+					binary = binary + rem * i;
+					i = i * 10;
+				}
+				return binary;
+			}
+	 public static long swaping(long bin) {
+		 
+		  long swaping1 = (((bin & 0x0F) << 4) | ((bin & 0xF0 >> 4)));
+		  return swaping1;
+	 }
+}
